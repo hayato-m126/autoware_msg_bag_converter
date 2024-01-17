03592cde935b62c72ce9f3bc561f0ed143948f7c
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-01-17 15:28:03 +0900

    chore: add main

--- autoware_msg_bag_converter/main.py
+++ autoware_msg_bag_converter/main.py
@@ -11,3 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import argparse
+from os.path import expandvars
+
+from autoware_msg_bag_converter.converter import convert_bag
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="path of input bag with autoware_auto_msgs")
+    parser.add_argument("output", help="path of output bag with autoware_msgs")
+    args = parser.parse_args()
+    convert_bag(expandvars(args.input), expandvars(args.output))
+
+
+if __name__ == "__main__":
+    main()
